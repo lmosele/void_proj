e32696f7148bbbff3d282813cf4e2315883aac98
Lucas Mosele <lucasmosele@gmail.com>
2023-02-02 15:58:23 -0500

    remove old version of tile based

--- Source/void_proj/Units/BaseUnitPawn.cpp
+++ Source/void_proj/Units/BaseUnitPawn.cpp
@@ -2,4 +2,112 @@
 
 
 #include "BaseUnitPawn.h"
+#include "Engine/World.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/FloatingPawnMovement.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/GameplayStatics.h"
+// #include "Tasks/AITask_MoveTo.h"
 
+#include "DrawDebugHelpers.h" 
+#include "NavigationSystem.h"
+
+// Sets default values
+ABaseUnitPawn::ABaseUnitPawn()
+{
+	// Set size for player capsule
+	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collider"));
+
+	RootComponent = CapsuleComp;
+
+    BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base Mesh"));
+	BaseMesh->SetupAttachment(CapsuleComp);
+
+	// Don't rotate character to camera direction
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+
+	// Create a camera boom...
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->SetUsingAbsoluteRotation(true); // Don't want arm to rotate when character does
+	CameraBoom->TargetArmLength = 800.f;
+	CameraBoom->SetRelativeRotation(FRotator(-60.f, 0.f, 0.f));
+	CameraBoom->bDoCollisionTest = false; // Don't want to pull camera in when it collides with level
+
+	// Create a camera...
+	TopDownCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("TopDownCamera"));
+	TopDownCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	TopDownCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+
+    MovementComponent = CreateDefaultSubobject<UFloatingPawnMovement>(TEXT("Movement Component"));
+    MovementComponent->Acceleration = 1.0f;
+    MovementComponent->MaxSpeed = 100.f;
+
+
+	// Activate ticking in order to update the cursor every frame.
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+
+}
+
+void ABaseUnitPawn::Tick(float DeltaSeconds)
+{
+    Super::Tick(DeltaSeconds);
+
+    DrawDebugSphere(GetWorld(), TargetLocation, 100.0f, 12, FColor::Magenta, true, -1.0f, SDPG_Foreground, 3.0f);
+}
+
+void ABaseUnitPawn::BeginPlay()
+{
+    Super::BeginPlay();
+    FVector CurrentLocation = GetActorLocation();
+    ZIndex = CurrentLocation.Z;
+
+	SetPatrol();
+    RotateTo();
+}
+
+
+// This isnt interping for some reason, need to figure it out.
+void ABaseUnitPawn::RotateTo()
+{
+    UE_LOG(LogTemp, Display, TEXT("Turning"));
+    SetActorRotation(FMath::RInterpTo(
+		GetActorRotation(), 
+		UKismetMathLibrary::FindLookAtRotation(GetActorLocation(), TargetLocation), 
+		UGameplayStatics::GetWorldDeltaSeconds(this), 
+		0.7f));
+}
+
+void ABaseUnitPawn::SetPatrol()
+{
+    FVector Result;
+	if (ABaseUnitPawn::GetRandomPointInRadius(GetActorLocation(), PatrolRadius, Result)) {
+		TargetLocation = Result;
+        // AAIController* UnitAIController = AITask->GetAIController();
+        // AITask->AIMoveTo(UnitAIController, TargetLocation);
+	}
+}
+
+
+bool ABaseUnitPawn::GetRandomPointInRadius(const FVector& Origin, float Radius, FVector& OutResult)
+{	
+	UNavigationSystemV1* NavSys = FNavigationSystem::GetCurrent<UNavigationSystemV1>(GetWorld());
+	if (!NavSys)
+	{
+		return false;
+	}
+
+	FVector Result;
+	bool bSuccess = NavSys->K2_GetRandomReachablePointInRadius(GetWorld(), Origin, Result, PatrolRadius);
+
+	OutResult = Result;
+	return bSuccess;
+}
--- Source/void_proj/Units/BaseUnitPawn.h
+++ Source/void_proj/Units/BaseUnitPawn.h
@@ -3,15 +3,63 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/DefaultPawn.h"
+#include "GameFramework/Pawn.h"
 #include "BaseUnitPawn.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class VOID_PROJ_API ABaseUnitPawn : public ADefaultPawn
+class VOID_PROJ_API ABaseUnitPawn : public APawn
 {
 	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseUnitPawn();
+
+	// Called every frame.
+	virtual void Tick(float DeltaSeconds) override;
+
+	void OnSelected();
+	void RotateTo();
 	
+	/** Returns TopDownCameraComponent subobject **/
+	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
+	/** Returns CameraBoom subobject **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	FVector TargetLocation;
+
+	float SensorRange = 1000.f;
+	float PatrolRadius = 400.f;
+	float ZIndex;
+	float EngageDistance;
+	bool CanMove;
+	bool IsTurning;
+
+	void SetPatrol();
+	bool GetRandomPointInRadius(const FVector& Origin, float Radius, FVector& OutResult);
+
+	class  UAITask_MoveTo* AITask;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* BaseMesh;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	class UCapsuleComponent* CapsuleComp; 
+	/** Top down camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* TopDownCameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	class UFloatingPawnMovement* MovementComponent;
+
 };
